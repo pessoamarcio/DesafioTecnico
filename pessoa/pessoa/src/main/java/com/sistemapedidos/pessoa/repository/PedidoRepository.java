aed61ae77a075fc66f2def68c4e73e4b84a13c4c
Marcio Pessoa <pessoa-marcio@email.com>
2026-03-10 19:43:07 -0300

    continuação da implementação do pedido

---
+++ pessoa/pessoa/src/main/java/com/sistemapedidos/pessoa/repository/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.sistemapedidos.pessoa.repository;
+
+import com.sistemapedidos.pessoa.model.Pedido;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    @EntityGraph(attributePaths = {"cliente", "itens", "itens.produto"})
+    Optional<Pedido> findById(Long id);
+}
