291e6c58bb7554e1478e116b8ca15de1be65908b
Marcio Pessoa <pessoa-marcio@email.com>
2026-03-19 20:51:33 -0300

    testes unitarios

---
+++ pessoa/pessoa/src/test/java/com/sistemapedidos/pessoa/interfaces/ApiExceptionHandlerRegraNegocioTest.java
@@ -0,0 +1,31 @@
+package com.sistemapedidos.pessoa.interfaces;
+
+import com.sistemapedidos.pessoa.exception.RegraNegocioException;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ApiExceptionHandlerRegraNegocioTest {
+
+    private final ApiExceptionHandler handler = new ApiExceptionHandler();
+
+    @Test
+    void handleRegraNegocioDeveRetornarBadRequest() {
+        ResponseEntity<ApiExceptionHandler.ApiErrorResponse> response =
+                handler.handleRegraNegocio(new RegraNegocioException("regra"));
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("regra", response.getBody().mensagem());
+    }
+
+    @Test
+    void handleIllegalStateDeveRetornarBadRequest() {
+        ResponseEntity<ApiExceptionHandler.ApiErrorResponse> response =
+                handler.handleRegraNegocio(new IllegalStateException("ilegal"));
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("ilegal", response.getBody().mensagem());
+    }
+}
